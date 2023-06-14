ef74b1d39d8741773ff6df0355c4b4fbd1430f8e
Parsa <parsarasooliofficial@gmail.com>
2023-06-14 13:19:43 +0330

    update cli

--- Prac1/src/app/Components/details/details.module.ts
+++ Prac1/src/app/Components/details/details.module.ts
@@ -3,9 +3,16 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatFormFieldModule, MatButtonModule, MatInputModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+  ],
 })
 export class DetailsModule {}
