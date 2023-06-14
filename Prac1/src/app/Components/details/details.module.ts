34c730d0cfd718f7fdc22910a89ef563621f7e71
Parsa <parsarasooliofficial@gmail.com>
2023-06-14 13:39:16 +0330

    Revert "update cli"
    This reverts commit ef74b1d39d8741773ff6df0355c4b4fbd1430f8e.

--- Prac1/src/app/Components/details/details.module.ts
+++ Prac1/src/app/Components/details/details.module.ts
@@ -3,16 +3,9 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    MatFormFieldModule,
-    MatButtonModule,
-    MatInputModule,
-    BrowserAnimationsModule,
-  ],
+  imports: [CommonModule, MatFormFieldModule, MatButtonModule, MatInputModule],
 })
 export class DetailsModule {}
