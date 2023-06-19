24d34f043c4947506a997598d7ff6a274c49e277
Parsa <parsarasooliofficial@gmail.com>
2023-06-19 18:20:39 +0330

    merge

--- Prac1/src/app/app.module.ts
+++ Prac1/src/app/app.module.ts
@@ -15,7 +15,7 @@ import { MaterialModule } from './material/material.module';
 import { TaskServiceService } from './services/task-service.service';
 import { HeadersInterceptor } from './interceptor/headers.interceptor';
 import { RouterModule } from '@angular/router';
-<<<<<<< Updated upstream
+
 import { GridComponent } from './Components/task-selector/grid/grid.component';
 import { MatInputModule } from '@angular/material/input';
 import { DetailsModule } from './Components/details/details.module';
@@ -25,16 +25,16 @@ import { MatOptionModule } from '@angular/material/core';
 import { HoverSpanDirective } from './directives/hover-span.directive';
 
 import { UppercaseDirective } from './directives/uppercase.directive';
-
-@NgModule({
-  declarations: [AppComponent, HoverSpanDirective, UppercaseDirective],
-=======
 import { UppercasePipe } from './pipes/uppercase.pipe';
-import { HoverSpanDirective } from './directives/hover-span.directive';
 
 @NgModule({
-  declarations: [AppComponent, UppercasePipe, HoverSpanDirective],
->>>>>>> Stashed changes
+  declarations: [
+    AppComponent,
+    UppercasePipe,
+    HoverSpanDirective,
+    UppercaseDirective,
+  ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -48,8 +48,6 @@ import { HoverSpanDirective } from './directives/hover-span.directive';
   ],
 
   exports: [HoverSpanDirective, UppercaseDirective],
-
-  exports: [HoverSpanDirective],
 
   providers: [
     TaskServiceService,
